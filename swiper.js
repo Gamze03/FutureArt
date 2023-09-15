1aa4e5cfd55a2df7182419edf58b357dcb1f7eaa
qemze <qemzemirkisiyeva90@gmail.com>
2023-09-15 12:56:34 +0400

    header bitdi

---
+++ swiper.js
@@ -0,0 +1,21 @@
+const BannerSwiper = new Swiper(".banner-swiper", {
+    slidesPerView: 1,
+    speed: 1000,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+
+        },
+    }
+    }
+);
