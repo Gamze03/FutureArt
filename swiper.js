926162794085027227e5a045f7aad293a82ac1e3
qemze <qemzemirkisiyeva90@gmail.com>
2023-09-19 14:52:21 +0400

    section project,service,about added

--- swiper.js
+++ swiper.js
@@ -13,6 +13,27 @@ const BannerSwiper = new Swiper(".banner-swiper", {
 
         1024: {
             slidesPerView: 1,
+
+        }
+    }
+    }
+);
+
+const ProjectSwiper = new Swiper(".project-swiper", {
+    slidesPerView: 1,
+    speed: 1000,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+
+        1024: {
+            slidesPerView: 3,
             spaceBetween: 40,
 
         },
